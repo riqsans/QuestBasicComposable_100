2c842bb17edf0309794afb0147c63a999338cd5b
riqsans <thrq88@gmail.com>
2025-10-16 18:36:05 +0700

    Menambahkan fungsi TataLetakColumn untuk menampilkan teks vertikal

--- app/src/main/java/com/example/tataletak/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/Tataletak.kt
@@ -1,4 +1,16 @@
-package com.example.tataletak
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
-class Tataletak {
+@Composable
+fun TataLetakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
 }
