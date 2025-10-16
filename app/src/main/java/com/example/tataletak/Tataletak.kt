ca7946e4c86c62ced6a44d10f14577bf90460049
riqsans <thrq88@gmail.com>
2025-10-16 18:38:37 +0700

    Menambahkan fungsi TataLetakRowColumn dengan dua kolom sejajar

--- app/src/main/java/com/example/tataletak/Tataletak.kt
+++ app/src/main/java/com/example/tataletak/Tataletak.kt
@@ -70,3 +70,22 @@ fun TataLetakColumnRow(modifier: Modifier) {
         }
     }
 }
+
+@Composable
+fun TataLetakRowColumn(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
+        Column {
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "Komponen3Kolom2")
+        }
+    }
+}
